9a9ea71fe764e06884ec2fc869aac6ae4f7f19f1
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-07-19 16:22:14 +0300

    edit android main

--- tolab/android/app/src/main/kotlin/com/example/tolab/MainActivity.kt
+++ tolab/android/app/src/main/kotlin/com/example/tolab/MainActivity.kt
@@ -2,4 +2,6 @@ package com.example.tolab
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+    // This class is intentionally left empty. It serves as the entry point for the Flutter application.
+}
