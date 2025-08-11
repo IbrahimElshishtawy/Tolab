775e5cc09350fdfca56e932b4a49a8ac9a27057d
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-08-11 12:18:37 +0300

    .

---
+++ tolab/lib/page/subjects/presentation/tabs/links_tab.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LinksTab extends StatelessWidget {
+  const LinksTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'روابط المادة',
+        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
