18ac76729c1f72163f88a7209221438986b9c112
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-11-18 14:58:08 +0200

    Update user_model.dart

--- eduhub/lib/features/auth/data/models/user_model.dart
+++ eduhub/lib/features/auth/data/models/user_model.dart
@@ -1,3 +1,5 @@
+import 'package:eduhub/features/auth/domain/entities/user.dart';
+
 class UserModel {
   final String id;
   final String fullname;
@@ -12,3 +14,10 @@ class UserModel {
 }
 
 // mdel user using dart
+
+extension UserModelMapper on UserModel {
+  UserEntity toEntity() {
+    return UserEntity(id: id, fullName: fullname, email: email, role: role);
+  }
+}
+//3.1. Mapper لـ Entity
