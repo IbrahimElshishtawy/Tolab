c80c62d469c961c2afb10c2ca61fe3693a399a1b
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-11-18 14:37:00 +0200

    Update user_model.dart

---
+++ eduhub/lib/features/auth/data/models/user_model.dart
@@ -0,0 +1,14 @@
+class UserModel {
+  final String id;
+  final String fullname;
+  final String email;
+  final String role;
+  UserModel({
+    required this.id,
+    required this.fullname,
+    required this.email,
+    required this.role,
+  });
+}
+
+// mdel user using dart
