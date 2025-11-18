c24ed0567cc6cd3405f166165b595072d843c7a5
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-11-18 23:20:10 +0200

    Update user_model.dart

--- eduhub/lib/features/auth/data/models/user_model.dart
+++ eduhub/lib/features/auth/data/models/user_model.dart
@@ -1,3 +1,5 @@
+// features/auth/data/models/user_model.dart
+
 import 'package:eduhub/features/auth/domain/entities/user.dart';
 
 class UserModel {
@@ -5,19 +7,32 @@ class UserModel {
   final String fullname;
   final String email;
   final String role;
+
   UserModel({
     required this.id,
     required this.fullname,
     required this.email,
     required this.role,
   });
-}
 
-// mdel user using dart
+  /// تحويل الـ Model لـ JSON (اختياري)
+  Map<String, dynamic> toJson() {
+    return {'id': id, 'fullname': fullname, 'email': email, 'role': role};
+  }
+
+  /// Factory لو جبنا داتا من API لاحقاً
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      id: json['id'],
+      fullname: json['fullname'],
+      email: json['email'],
+      role: json['role'],
+    );
+  }
+}
 
 extension UserModelMapper on UserModel {
   UserEntity toEntity() {
     return UserEntity(id: id, fullName: fullname, email: email, role: role);
   }
 }
-//3.1. Mapper لـ Entity
