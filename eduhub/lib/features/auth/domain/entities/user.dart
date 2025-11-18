e5efc18d2f3cb2daeec12d91293f0e10953cda6d
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-11-18 14:58:14 +0200

    Update user.dart

---
+++ eduhub/lib/features/auth/domain/entities/user.dart
@@ -0,0 +1,13 @@
+class UserEntity {
+  final String id;
+  final String fullName;
+  final String email;
+  final String role;
+
+  const UserEntity({
+    required this.id,
+    required this.fullName,
+    required this.email,
+    required this.role,
+  });
+}
