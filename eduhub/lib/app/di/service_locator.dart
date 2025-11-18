09e62ef257499d32a9c7d0318b2f603fefeaba6e
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-11-18 23:20:00 +0200

    Update service_locator.dart

--- eduhub/lib/app/di/service_locator.dart
+++ eduhub/lib/app/di/service_locator.dart
@@ -1,4 +1,9 @@
 import 'package:eduhub/features/auth/data/datasources/auth_fake_datasource.dart';
+import 'package:eduhub/features/auth/data/repositories/auth_repository_impl.dart';
+import 'package:eduhub/features/auth/domain/repositories/auth_repository.dart';
+import 'package:eduhub/features/auth/domain/usecases/get_current_user_usecase.dart';
+import 'package:eduhub/features/auth/domain/usecases/login_usecase.dart';
+import 'package:eduhub/features/auth/domain/usecases/logout_usecase.dart';
 import 'package:get_it/get_it.dart';
 
 final sl = GetIt.instance;
@@ -9,9 +14,9 @@ Future<void> initDependencies() async {
 
   // Repositories
   sl.registerLazySingleton<AuthRepository>(
-    () => AuthRepositoryImpl(sl<AuthFakeDataSource>()),
+    () =>
+        AuthRepositoryImpl(sl<AuthFakeDataSource>(), remote: sl(), local: sl()),
   );
-
   // UseCases
   sl.registerLazySingleton(() => LoginUseCase(sl<AuthRepository>()));
   sl.registerLazySingleton(() => GetCurrentUserUseCase(sl<AuthRepository>()));
