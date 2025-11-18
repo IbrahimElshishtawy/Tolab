b0420ff4cf94760e3ce9d5163de234c75d9fc82c
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-11-18 20:36:20 +0200

    Update logout_usecase.dart

---
+++ eduhub/lib/features/auth/domain/usecases/logout_usecase.dart
@@ -0,0 +1,13 @@
+// features/auth/domain/usecases/logout_usecase.dart
+
+import '../repositories/auth_repository.dart';
+
+class LogoutUseCase {
+  final AuthRepository repository;
+
+  LogoutUseCase(this.repository);
+
+  Future<void> call() {
+    return repository.logout();
+  }
+}
