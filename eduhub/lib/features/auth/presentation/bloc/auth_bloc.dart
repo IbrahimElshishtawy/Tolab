0b7f45db582d438ac53345479e65d752ea3e7043
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-11-18 20:29:11 +0200

    Update auth_bloc.dart

--- eduhub/lib/features/auth/presentation/bloc/auth_bloc.dart
+++ eduhub/lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,30 +1,40 @@
-// features/auth/presentation/cubit/auth_cubit.dart
+// features/auth/presentation/bloc/auth_bloc.dart
 
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'auth_event.dart';
+import 'auth_state.dart';
 
 import '../../domain/usecases/login_usecase.dart';
 import '../../domain/usecases/get_current_user_usecase.dart';
 import '../../domain/usecases/logout_usecase.dart';
 import '../../domain/entities/user.dart';
-import 'auth_state.dart';
 
-class AuthCubit extends Cubit<AuthState> {
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
   final LoginUseCase _loginUseCase;
   final GetCurrentUserUseCase _getCurrentUserUseCase;
   final LogoutUseCase _logoutUseCase;
 
-  AuthCubit({
+  AuthBloc({
     required LoginUseCase loginUseCase,
-    required GetCurrentUserUseCase getCurrentUserUseCase,
-    required LogoutUseCase logoutUseCase,
-  }) : _loginUseCase = loginUseCase,
-       _getCurrentUserUseCase = getCurrentUserUseCase,
-       _logoutUseCase = logoutUseCase,
-       super(const AuthInitial());
+    required GetCurrentUserUseCase getCurrentUserUsecase,
+    required LogoutUseCase logoutUsecase,
+  })  : _loginUseCase = loginUseCase,
+        _getCurrentUserUseCase = getCurrentUserUsecase,
+        _logoutUseCase = logoutUsecase,
+        super(const AuthInitial()) {
+    on<AppStarted>(_onAppStarted);
+    on<LoginRequested>(_onLoginRequested);
+    on<LogoutRequested>(_onLogoutRequested);
+  }
 
-  /// استدعِها في الـ Splash عشان يعرف هل في User مسجّل ولا لا
-  Future<void> checkAuthStatus() async {
+ 
+  Future<void> _onAppStarted(
+    AppStarted event,
+    Emitter<AuthState> emit,
+  ) async {
     emit(const AuthLoading());
+
     try {
       final UserEntity? user = await _getCurrentUserUseCase();
       if (user != null) {
@@ -37,21 +47,28 @@ class AuthCubit extends Cubit<AuthState> {
     }
   }
 
-  /// Login من شاشة الدخول
-  Future<void> login(String email, String password) async {
+  /// Login بالـ Fake Data
+  Future<void> _onLoginRequested(
+    LoginRequested event,
+    Emitter<AuthState> emit,
+  ) async {
     emit(const AuthLoading());
+
     try {
-      final user = await _loginUseCase(email, password);
+      final user =
+          await _loginUseCase(event.email, event.password);
       emit(AuthAuthenticated(user));
     } catch (e) {
       emit(AuthError(e.toString()));
-
       emit(const AuthUnauthenticated());
     }
   }
 
-  /// Logout من أي مكان (زر Logout)
-  Future<void> logout() async {
+  /// Logout
+  Future<void> _onLogoutRequested(
+    LogoutRequested event,
+    Emitter<AuthState> emit,
+  ) async {
     await _logoutUseCase();
     emit(const AuthUnauthenticated());
   }
