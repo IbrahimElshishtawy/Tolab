79b6108818badfb274e55661d016eea1d4e63373
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-07 13:51:42 +0200

    Create student_nav_item_data.dart

---
+++ eduhub/lib/apps/tolab_student_desktop/lib/src/presentation/desktop/dashboard/ui/student_nav_item_data.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class StudentNavItemData {
+  final IconData icon;
+  final String label;
+
+  const StudentNavItemData({required this.icon, required this.label});
+}
