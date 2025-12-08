899a4868ed406629c859c68b95bb6613c8a848e4
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-08 21:57:08 +0200

    Update store.dart

---
+++ eduhub/lib/apps/tolab_admin_panel/lib/src/state/store.dart
@@ -0,0 +1,15 @@
+import 'package:eduhub/apps/tolab_admin_panel/lib/src/core/api/Api_Service.dart';
+import 'package:redux/redux.dart';
+import 'app_state.dart';
+import 'reducers/app_reducer.dart';
+import 'auth/auth_middleware.dart';
+
+Store<AppState> createStore() {
+  final api = ApiService();
+
+  return Store<AppState>(
+    appReducer,
+    initialState: AppState.initial(),
+    middleware: [...authMiddleware(api)],
+  );
+}
