3b4226c5fd5718bf939ab7dffeacd7b300347c5e
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-08 21:56:45 +0200

    Create Api_Service.dart

---
+++ eduhub/lib/apps/tolab_admin_panel/lib/src/core/api/Api_Service.dart
@@ -0,0 +1,11 @@
+class ApiService {
+  Future<String> login(String email, String password) async {
+    await Future.delayed(Duration(seconds: 2)); // simulate server
+
+    if (email == "admin@tolab.com" && password == "123456") {
+      return "FAKE_TOKEN_123"; // simulated token
+    } else {
+      throw Exception("Invalid credentials");
+    }
+  }
+}
