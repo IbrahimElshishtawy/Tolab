66b98c68289f94ac3b77c33bfdc3becdf2f91e5e
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-08 21:42:04 +0200

    Create auth_middleware.dart

---
+++ eduhub/lib/apps/tolab_admin_panel/lib/src/state/auth/auth_middleware.dart
@@ -0,0 +1,22 @@
+import 'package:redux/redux.dart';
+import 'auth_actions.dart';
+
+import '../app_state.dart';
+
+List<Middleware<AppState>> authMiddleware(ApiService api) {
+  return [TypedMiddleware<AppState, LoginAction>(_login(api)).call];
+}
+
+Middleware<AppState> _login(ApiService api) {
+  return (Store<AppState> store, action, NextDispatcher next) async {
+    next(action);
+
+    try {
+      final token = await api.login(action.email, action.password);
+
+      store.dispatch(LoginSuccessAction(token));
+    } catch (e) {
+      store.dispatch(LoginFailedAction(e.toString()));
+    }
+  };
+}
