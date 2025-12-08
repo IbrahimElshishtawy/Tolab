3d5af5cca62ea5322cda6632bfc27650ef0b30ce
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-08 21:56:53 +0200

    Update auth_reducer.dart

--- eduhub/lib/apps/tolab_admin_panel/lib/src/state/auth/auth_reducer.dart
+++ eduhub/lib/apps/tolab_admin_panel/lib/src/state/auth/auth_reducer.dart
@@ -3,19 +3,19 @@ import 'auth_state.dart';
 
 AuthState authReducer(AuthState state, dynamic action) {
   if (action is LoginAction) {
-    return state.copyWith(isLoading: true, error: null);
+    return state.copyWith(isloading: true, errorMessage: null);
   }
 
   if (action is LoginSuccessAction) {
     return state.copyWith(
-      isLoading: false,
-      isLoggedIn: true,
+      isloading: false,
+      isloadingIn: true,
       token: action.token,
     );
   }
 
   if (action is LoginFailedAction) {
-    return state.copyWith(isLoading: false, error: action.message);
+    return state.copyWith(isloading: false, errorMessage: action.message);
   }
 
   if (action is LogoutAction) {
