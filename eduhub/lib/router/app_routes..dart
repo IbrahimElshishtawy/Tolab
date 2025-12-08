4815d8505fd5be9462e5ac3e04eb0b682decd146
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-08 23:17:37 +0200

    Update app_routes..dart

--- eduhub/lib/router/app_routes..dart
+++ eduhub/lib/router/app_routes..dart
@@ -2,7 +2,7 @@
 // ignore_for_file: file_names
 
 import 'package:eduhub/apps/tolab_admin_panel/lib/src/presentation/features/dashboard/pages/Dashboard_Page.dart';
-import 'package:eduhub/apps/tolab_student_desktop/lib/src/presentation/desktop/dashboard/layouts/student_desktop_home_page.dart';
+//import 'package:eduhub/apps/tolab_student_desktop/lib/src/presentation/desktop/dashboard/layouts/student_desktop_home_page.dart';
 import 'package:flutter/material.dart';
 import '../spa/Splach_screen.dart';
 
@@ -17,11 +17,10 @@ class AppRouter {
     switch (settings.name) {
       case AppRoutes.splash:
         return MaterialPageRoute(builder: (_) => const Splashscreen());
-
-      case AppRoutes.studentDesktopHome:
-        return MaterialPageRoute(
-          builder: (_) => const StudentDesktopHomePage(),
-        );
+      // case AppRoutes.studentDesktopHome:
+      //   return MaterialPageRoute(
+      //     builder: (_) => const StudentDesktopHomePage(),
+      //   );
       case AppRoutes.dashboard:
         return MaterialPageRoute(builder: (_) => const DashboardPage());
       default:
