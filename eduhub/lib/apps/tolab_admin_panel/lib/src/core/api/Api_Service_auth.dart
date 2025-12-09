c8c1b5c21a58ee7b0025e16e408cf387a8c3ca99
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-10 00:05:52 +0200

    Update Api_Service_auth.dart

--- eduhub/lib/apps/tolab_admin_panel/lib/src/core/api/Api_Service_auth.dart
+++ eduhub/lib/apps/tolab_admin_panel/lib/src/core/api/Api_Service_auth.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: file_names
 
-class ApiService {
+class ApiServiceAuth {
   Future<String> login(String email, String password) async {
     await Future.delayed(Duration(seconds: 2)); // simulate server
 
