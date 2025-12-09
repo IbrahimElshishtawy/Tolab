8148810f39135c770035df08ec1b81348422198f
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-09 16:45:54 +0200

    Update app_state.dart

--- eduhub/lib/apps/tolab_admin_panel/lib/src/state/app_state.dart
+++ eduhub/lib/apps/tolab_admin_panel/lib/src/state/app_state.dart
@@ -1,15 +1,20 @@
 import 'package:eduhub/apps/tolab_admin_panel/lib/src/state/auth/auth_state.dart';
+import 'package:eduhub/apps/tolab_admin_panel/lib/src/state/dashboard/dashboard_state.dart';
 
 class AppState {
   final AuthState auth;
+  final DashboardState dashboard;
 
-  AppState({required this.auth});
+  AppState({required this.auth, required this.dashboard});
 
   factory AppState.initial() {
-    return AppState(auth: AuthState.initial());
+    return AppState(
+      auth: AuthState.initial(),
+      dashboard: DashboardState.initial(),
+    );
   }
 
   AppState copyWith({AuthState? auth}) {
-    return AppState(auth: auth ?? this.auth);
+    return AppState(auth: auth ?? this.auth, dashboard: dashboard);
   }
 }
