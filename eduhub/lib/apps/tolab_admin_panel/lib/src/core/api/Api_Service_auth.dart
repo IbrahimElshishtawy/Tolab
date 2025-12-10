55f5214233cdf7785171fc4e4c26257243ff708e
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-10 21:19:26 +0200

    Update Api_Service_auth.dart

--- eduhub/lib/apps/tolab_admin_panel/lib/src/core/api/Api_Service_auth.dart
+++ eduhub/lib/apps/tolab_admin_panel/lib/src/core/api/Api_Service_auth.dart
@@ -1,13 +1,22 @@
 // ignore_for_file: file_names
 
+import 'package:eduhub/fake_data/data.dart';
+
 class ApiServiceAuth {
   Future<String> login(String email, String password) async {
-    await Future.delayed(Duration(seconds: 2)); // simulate server
+    await Future.delayed(const Duration(milliseconds: 800)); // simulate server
 
-    if (email == "admin@tolab.com" && password == "123456") {
-      return "FAKE_TOKEN_123"; // simulated token
-    } else {
-      throw Exception("Invalid credentials");
+    // البحث عن المستخدم داخل fake_data
+    final admin = admins.firstWhere(
+      (a) => a["email"] == email && a["password"] == password,
+      orElse: () => {},
+    );
+
+    if (admin.isNotEmpty) {
+      // رجّع توكن وهمي (في الحقيقة يمكنك إرجاع user object وليس token فقط)
+      return "TOKEN_${admin['email']}";
     }
+
+    throw Exception("Invalid credentials");
   }
 }
