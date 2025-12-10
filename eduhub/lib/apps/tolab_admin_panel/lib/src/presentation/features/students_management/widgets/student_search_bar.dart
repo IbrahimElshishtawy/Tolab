71ebb3f109a4bac2f136b21c5861570d113e9272
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-10 18:47:18 +0200

    Create student_search_bar.dart

---
+++ eduhub/lib/apps/tolab_admin_panel/lib/src/presentation/features/students_management/widgets/student_search_bar.dart
@@ -0,0 +1,27 @@
+// TODO Implement this library.
+import 'package:flutter/material.dart';
+
+class StudentSearchBar extends StatelessWidget {
+  final ValueChanged<String> onChanged;
+
+  const StudentSearchBar({super.key, required this.onChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      onChanged: onChanged,
+      style: const TextStyle(color: Colors.white),
+      decoration: InputDecoration(
+        hintText: "Search Students...",
+        hintStyle: const TextStyle(color: Colors.white54),
+        prefixIcon: const Icon(Icons.search, color: Colors.white),
+        filled: true,
+        fillColor: Colors.white.withOpacity(.06),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(14),
+          borderSide: BorderSide.none,
+        ),
+      ),
+    );
+  }
+}
