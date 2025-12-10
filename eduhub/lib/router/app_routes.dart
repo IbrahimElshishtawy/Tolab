e8af52feacf3d49cdf83f1b555b8b9a3033a2b77
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-10 16:24:22 +0200

    Update app_routes.dart

--- eduhub/lib/router/app_routes.dart
+++ eduhub/lib/router/app_routes.dart
@@ -1,15 +1,16 @@
 // lib/router/app_routes.dart
-// ignore_for_file: file_names
+// ignore_for_file: constant_identifier_names, file_names
 
+import 'package:eduhub/apps/tolab_admin_panel/lib/src/presentation/features/auth/pages/login_page.dart';
 import 'package:eduhub/apps/tolab_admin_panel/lib/src/presentation/features/dashboard/pages/Dashboard_Page.dart';
-//import 'package:eduhub/apps/tolab_student_desktop/lib/src/presentation/desktop/dashboard/layouts/student_desktop_home_page.dart';
 import 'package:flutter/material.dart';
+
 import '../spa/Splach_screen.dart';
 
 class AppRoutes {
   static const String splash = '/';
-  static const String studentDesktopHome = '/student/desktop/home';
   static const String dashboard = '/dashboard';
+  static const String login_dashboard = '/login_Dashboard';
 }
 
 class AppRouter {
@@ -17,12 +18,12 @@ class AppRouter {
     switch (settings.name) {
       case AppRoutes.splash:
         return MaterialPageRoute(builder: (_) => const Splashscreen());
-      // case AppRoutes.studentDesktopHome:
-      //   return MaterialPageRoute(
-      //     builder: (_) => const StudentDesktopHomePage(),
-      //   );
+
       case AppRoutes.dashboard:
         return MaterialPageRoute(builder: (_) => const DashboardPage());
+
+      case AppRoutes.login_dashboard:
+        return MaterialPageRoute(builder: (_) => const LoginPage_Dashboard());
       default:
         return MaterialPageRoute(builder: (_) => const Splashscreen());
     }
