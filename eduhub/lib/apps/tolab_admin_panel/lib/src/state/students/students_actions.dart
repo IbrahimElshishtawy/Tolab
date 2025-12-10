371d6f8d68ae91c9a01abd8808035c276825293e
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-10 21:30:15 +0200

    Update students_actions.dart

--- eduhub/lib/apps/tolab_admin_panel/lib/src/state/students/students_actions.dart
+++ eduhub/lib/apps/tolab_admin_panel/lib/src/state/students/students_actions.dart
@@ -2,12 +2,10 @@ class LoadStudentsAction {}
 
 class StudentsLoadedAction {
   final List<Map<String, dynamic>> students;
-
   StudentsLoadedAction(this.students);
 }
 
 class StudentsFailedAction {
   final String error;
-
   StudentsFailedAction(this.error);
 }
