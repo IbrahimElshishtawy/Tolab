7eadcbe167b2be2427337d09a335e2c6990281dc
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-10 21:30:11 +0200

    Update Api_Service_auth.dart

--- eduhub/lib/apps/tolab_admin_panel/lib/src/core/api/Api_Service_auth.dart
+++ eduhub/lib/apps/tolab_admin_panel/lib/src/core/api/Api_Service_auth.dart
@@ -5,15 +5,12 @@ import 'package:eduhub/fake_data/data.dart';
 class ApiServiceAuth {
   Future<String> login(String email, String password) async {
     await Future.delayed(const Duration(milliseconds: 800)); // simulate server
-
-    // البحث عن المستخدم داخل fake_data
     final admin = admins.firstWhere(
       (a) => a["email"] == email && a["password"] == password,
       orElse: () => {},
     );
 
     if (admin.isNotEmpty) {
-      // رجّع توكن وهمي (في الحقيقة يمكنك إرجاع user object وليس token فقط)
       return "TOKEN_${admin['email']}";
     }
 
