dab621cd5c00dd935f34f8eee3b840601b22f84d
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-10 18:56:39 +0200

    Update app_reducer.dart

--- eduhub/lib/apps/tolab_admin_panel/lib/src/state/reducers/app_reducer.dart
+++ eduhub/lib/apps/tolab_admin_panel/lib/src/state/reducers/app_reducer.dart
@@ -1,10 +1,12 @@
 import 'package:eduhub/apps/tolab_admin_panel/lib/src/state/app_state.dart';
 import 'package:eduhub/apps/tolab_admin_panel/lib/src/state/auth/auth_reducer.dart';
 import 'package:eduhub/apps/tolab_admin_panel/lib/src/state/dashboard/dashboard_reducer.dart';
+import 'package:eduhub/apps/tolab_admin_panel/lib/src/state/students/students_reducer.dart';
 
 AppState appReducer(AppState state, dynamic action) {
   return AppState(
     auth: authReducer(state.auth, action),
     dashboard: dashboardReducer(state.dashboard, action),
+    students: studentsReducer(state.students, action),
   );
 }
