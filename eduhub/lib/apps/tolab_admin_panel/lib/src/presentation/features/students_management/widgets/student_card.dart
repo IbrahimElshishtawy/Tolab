f616a3511967ff3e741dab228f71f246099bbd89
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-10 18:47:11 +0200

    Create student_card.dart

---
+++ eduhub/lib/apps/tolab_admin_panel/lib/src/presentation/features/students_management/widgets/student_card.dart
@@ -0,0 +1,51 @@
+// TODO Implement this library.
+import 'package:flutter/material.dart';
+import '../pages/student_details_page.dart';
+
+class StudentCard extends StatelessWidget {
+  final Map<String, dynamic> student;
+
+  const StudentCard({super.key, required this.student});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(builder: (_) => StudentDetailsPage(student: student)),
+      ),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: Colors.white.withOpacity(.06),
+          borderRadius: BorderRadius.circular(14),
+          border: Border.all(color: Colors.white10),
+        ),
+        child: Row(
+          children: [
+            CircleAvatar(
+              radius: 26,
+              backgroundColor: Colors.blueAccent.withOpacity(.3),
+              child: const Icon(Icons.person, color: Colors.white, size: 28),
+            ),
+            const SizedBox(width: 14),
+            Expanded(
+              child: Text(
+                student["name"],
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 17,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+            Text(
+              "Year ${student["year"]}",
+              style: const TextStyle(color: Colors.white70),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
