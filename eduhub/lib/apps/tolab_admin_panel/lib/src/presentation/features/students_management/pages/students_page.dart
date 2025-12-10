4c61889acaf0f5938eaa789b7b21d3b736439094
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-10 18:47:06 +0200

    Create students_page.dart

---
+++ eduhub/lib/apps/tolab_admin_panel/lib/src/presentation/features/students_management/pages/students_page.dart
@@ -0,0 +1,74 @@
+import 'package:eduhub/fake_data/data.dart';
+import 'package:flutter/material.dart';
+import '../widgets/student_card.dart';
+import '../widgets/student_search_bar.dart';
+import '../widgets/student_filter_bar.dart';
+
+class StudentsPage extends StatefulWidget {
+  const StudentsPage({super.key});
+
+  @override
+  State<StudentsPage> createState() => _StudentsPageState();
+}
+
+class _StudentsPageState extends State<StudentsPage> {
+  String searchQuery = "";
+  String? selectedDepartment;
+  int? selectedYear;
+
+  @override
+  Widget build(BuildContext context) {
+    final filtered = students.where((student) {
+      final matchesSearch = student["name"].toString().toLowerCase().contains(
+        searchQuery.toLowerCase(),
+      );
+
+      final matchesDepartment =
+          selectedDepartment == null ||
+          student["department"] == selectedDepartment;
+
+      final matchesYear =
+          selectedYear == null || student["year"] == selectedYear;
+
+      return matchesSearch && matchesDepartment && matchesYear;
+    }).toList();
+
+    return Scaffold(
+      backgroundColor: const Color(0xFF0F172A),
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          children: [
+            StudentSearchBar(onChanged: (v) => setState(() => searchQuery = v)),
+
+            const SizedBox(height: 12),
+
+            StudentFilterBar(
+              selectedDepartment: selectedDepartment,
+              selectedYear: selectedYear,
+              onDepartmentChange: (d) => setState(() => selectedDepartment = d),
+              onYearChange: (y) => setState(() => selectedYear = y),
+            ),
+
+            const SizedBox(height: 22),
+
+            Expanded(
+              child: GridView.builder(
+                itemCount: filtered.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  mainAxisSpacing: 20,
+                  crossAxisSpacing: 20,
+                  childAspectRatio: 1.9,
+                ),
+                itemBuilder: (_, i) {
+                  return StudentCard(student: filtered[i]);
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
