672615cb07a2f1413711f2debb871833f54bde2c
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-11 18:50:10 +0200

    Update students_selectors.dart

--- eduhub/lib/apps/tolab_admin_panel/lib/src/state/students/students_selectors.dart
+++ eduhub/lib/apps/tolab_admin_panel/lib/src/state/students/students_selectors.dart
@@ -1,11 +1,5 @@
 import '../app_state.dart';
-import 'students_state.dart';
 
-StudentsState studentsSelector(AppState state) => state.students;
-
-List<Map<String, dynamic>> allStudentsSelector(AppState state) =>
-    state.students.students;
-
-bool studentsLoadingSelector(AppState state) => state.students.isLoading;
-
-String? studentsErrorSelector(AppState state) => state.students.error;
+studentsSelector(AppState state) => state.students.students;
+studentsIsLoadingSelector(AppState state) => state.students.isLoading;
+studentsErrorSelector(AppState state) => state.students.error;
