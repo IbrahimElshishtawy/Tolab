89f18cc0b53e03af1a1914b1f1050f67689f1b8c
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-11 18:50:53 +0200

    Create structure_middleware.dart

---
+++ src/state/academic_structure/structure_middleware.dart
@@ -0,0 +1,21 @@
+﻿import 'package:redux/redux.dart';
+import '../app_state.dart';
+import 'structure_actions.dart';
+import '../../../fake_data/data.dart';
+
+List<Middleware<AppState>> createstructureMiddleware() {
+  return [
+    TypedMiddleware<AppState, LoadstructureAction>(_loadstructure()),
+  ];
+}
+
+Middleware<AppState> _loadstructure() {
+  return (Store<AppState> store, action, NextDispatcher next) async {
+    next(action);
+    try {
+      store.dispatch(structureLoadedAction(fakeData));
+    } catch (e) {
+      store.dispatch(structureFailedAction(e.toString()));
+    }
+  };
+}
