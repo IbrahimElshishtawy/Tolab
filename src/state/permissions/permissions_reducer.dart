29aed4a139cc92baa135357cf567c49bf39b4a95
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-11 18:52:02 +0200

    Create permissions_reducer.dart

---
+++ src/state/permissions/permissions_reducer.dart
@@ -0,0 +1,18 @@
+﻿import 'permissions_actions.dart';
+import 'permissions_state.dart';
+
+permissionsState permissionsReducer(permissionsState state, dynamic action) {
+  if (action is LoadpermissionsAction) {
+    return state.copyWith(isLoading: true, error: null);
+  }
+
+  if (action is permissionsLoadedAction) {
+    return state.copyWith(isLoading: false, data: action.data);
+  }
+
+  if (action is permissionsFailedAction) {
+    return state.copyWith(isLoading: false, error: action.error);
+  }
+
+  return state;
+}
