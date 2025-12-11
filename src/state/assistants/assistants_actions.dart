0fe7be1c7d33599813e8c938f50f2006b40e458c
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-11 18:51:04 +0200

    Create assistants_actions.dart

---
+++ src/state/assistants/assistants_actions.dart
@@ -0,0 +1,11 @@
+﻿class LoadassistantsAction {}
+
+class assistantsLoadedAction {
+  final dynamic data;
+  assistantsLoadedAction(this.data);
+}
+
+class assistantsFailedAction {
+  final String error;
+  assistantsFailedAction(this.error);
+}
