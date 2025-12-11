bc314ce3d08bc97fb913cb94b5e56ca443010076
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-11 18:50:36 +0200

    Create assistants_page.dart

---
+++ src/presentation/features/assistants_management/pages/assistants_page.dart
@@ -0,0 +1,19 @@
+﻿import 'package:flutter/material.dart';
+
+class assistants_pagePage extends StatelessWidget {
+  const assistants_pagePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF0F172A),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF1E293B),
+        title: const Text('assistants_page', style: TextStyle(color: Colors.white)),
+      ),
+      body: const Center(
+        child: Text('assistants_page Page', style: TextStyle(color: Colors.white)),
+      ),
+    );
+  }
+}
