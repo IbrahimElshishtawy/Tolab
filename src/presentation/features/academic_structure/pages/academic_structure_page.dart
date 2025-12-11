bfed2899a56cd48908963d2ba180a083c876c0f4
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-11 18:50:23 +0200

    Create academic_structure_page.dart

---
+++ src/presentation/features/academic_structure/pages/academic_structure_page.dart
@@ -0,0 +1,19 @@
+﻿import 'package:flutter/material.dart';
+
+class academic_structure_pagePage extends StatelessWidget {
+  const academic_structure_pagePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF0F172A),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF1E293B),
+        title: const Text('academic_structure_page', style: TextStyle(color: Colors.white)),
+      ),
+      body: const Center(
+        child: Text('academic_structure_page Page', style: TextStyle(color: Colors.white)),
+      ),
+    );
+  }
+}
