83b773f0f39be9320de22560f576c971ab0d4cd0
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-11 18:49:31 +0200

    Create doctors_middleware.dart

---
+++ eduhub/lib/apps/tolab_admin_panel/lib/src/state/doctors/doctors_middleware.dart
@@ -0,0 +1,22 @@
+// TODO Implement this library.
+import 'package:redux/redux.dart';
+import '../../core/api/api_service_doctors.dart';
+import '../app_state.dart';
+import 'doctors_actions.dart';
+
+List<Middleware<AppState>> createDoctorsMiddleware(ApiServiceDoctors api) {
+  return [TypedMiddleware<AppState, LoadDoctorsAction>(_loadDoctors(api))];
+}
+
+Middleware<AppState> _loadDoctors(ApiServiceDoctors api) {
+  return (Store<AppState> store, action, NextDispatcher next) async {
+    next(action);
+
+    try {
+      final data = await api.fetchDoctors();
+      store.dispatch(DoctorsLoadedAction(data));
+    } catch (e) {
+      store.dispatch(DoctorsFailedAction(e.toString()));
+    }
+  };
+}
