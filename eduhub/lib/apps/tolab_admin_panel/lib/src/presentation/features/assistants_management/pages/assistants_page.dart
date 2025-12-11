1d85c072252c06462f4a8b2f89b19c1f5ad15469
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-11 21:01:57 +0200

    Update assistants_page.dart

--- eduhub/lib/apps/tolab_admin_panel/lib/src/presentation/features/assistants_management/pages/assistants_page.dart
+++ eduhub/lib/apps/tolab_admin_panel/lib/src/presentation/features/assistants_management/pages/assistants_page.dart
@@ -1,4 +1,6 @@
-﻿import 'package:flutter/material.dart';
+﻿// ignore_for_file: camel_case_types
+
+import 'package:flutter/material.dart';
 
 class assistants_pagePage extends StatelessWidget {
   const assistants_pagePage({super.key});
@@ -9,10 +11,16 @@ class assistants_pagePage extends StatelessWidget {
       backgroundColor: const Color(0xFF0F172A),
       appBar: AppBar(
         backgroundColor: const Color(0xFF1E293B),
-        title: const Text('assistants_page', style: TextStyle(color: Colors.white)),
+        title: const Text(
+          'assistants_page',
+          style: TextStyle(color: Colors.white),
+        ),
       ),
       body: const Center(
-        child: Text('assistants_page Page', style: TextStyle(color: Colors.white)),
+        child: Text(
+          'assistants_page Page',
+          style: TextStyle(color: Colors.white),
+        ),
       ),
     );
   }
