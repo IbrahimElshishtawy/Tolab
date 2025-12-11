c317ee8c91d920026d7c2f0c487bf7f96aedf3e0
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-11 18:51:36 +0200

    Create assistants_state.dart

---
+++ src/state/assistants/assistants_state.dart
@@ -0,0 +1,22 @@
+﻿class assistantsState {
+  final bool isLoading;
+  final dynamic data;
+  final String? error;
+
+  assistantsState({
+    required this.isLoading,
+    required this.data,
+    this.error,
+  });
+
+  factory assistantsState.initial() =>
+      assistantsState(isLoading: false, data: null);
+
+  assistantsState copyWith({bool? isLoading, dynamic data, String? error}) {
+    return assistantsState(
+      isLoading: isLoading ?? this.isLoading,
+      data: data ?? this.data,
+      error: error,
+    );
+  }
+}
