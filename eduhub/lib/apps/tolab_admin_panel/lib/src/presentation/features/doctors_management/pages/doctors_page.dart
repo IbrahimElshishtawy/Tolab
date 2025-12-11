875fbc2128dc2ea01ec43d4395ada0370ddf60fe
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-11 20:33:04 +0200

    Create doctors_page.dart

---
+++ eduhub/lib/apps/tolab_admin_panel/lib/src/presentation/features/doctors_management/pages/doctors_page.dart
@@ -0,0 +1,24 @@
+﻿// ignore_for_file: camel_case_types
+
+import 'package:flutter/material.dart';
+
+class doctors_pagePage extends StatelessWidget {
+  const doctors_pagePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF0F172A),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF1E293B),
+        title: const Text(
+          'doctors_page',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: const Center(
+        child: Text('doctors_page Page', style: TextStyle(color: Colors.white)),
+      ),
+    );
+  }
+}
