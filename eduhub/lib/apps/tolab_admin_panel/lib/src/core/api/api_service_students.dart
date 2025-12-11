647cefa4b5b41bcb64fb46042e80b8e473a964b0
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-11 20:32:49 +0200

    Update api_service_students.dart

--- eduhub/lib/apps/tolab_admin_panel/lib/src/core/api/api_service_students.dart
+++ eduhub/lib/apps/tolab_admin_panel/lib/src/core/api/api_service_students.dart
@@ -1,1 +1,40 @@
-// TODO Implement this library.
+import 'package:eduhub/fake_data/data.dart';
+
+class ApiServiceStudents {
+  Future<List<Map<String, dynamic>>> fetchStudents() async {
+    await Future.delayed(const Duration(milliseconds: 300));
+    return students;
+  }
+
+  Future<Map<String, dynamic>> fetchStudentById(String id) async {
+    await Future.delayed(const Duration(milliseconds: 300));
+
+    try {
+      return students.firstWhere((s) => s["student_id"] == id);
+    } catch (e) {
+      throw Exception("Student not found");
+    }
+  }
+
+  Future<List<Map<String, dynamic>>> searchStudents(String query) async {
+    await Future.delayed(const Duration(milliseconds: 240));
+
+    query = query.toLowerCase();
+
+    return students.where((student) {
+      return student["name"].toString().toLowerCase().contains(query) ||
+          student["email"].toLowerCase().contains(query) ||
+          student["student_id"].toLowerCase().contains(query);
+    }).toList();
+  }
+
+  Future<List<String>> fetchDepartments() async {
+    await Future.delayed(const Duration(milliseconds: 200));
+
+    final set = <String>{};
+    for (var s in students) {
+      set.add(s["department"]);
+    }
+    return set.toList();
+  }
+}
