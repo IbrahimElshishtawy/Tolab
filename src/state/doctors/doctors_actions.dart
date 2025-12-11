a6ae7169beac3e937ab77346c2e260ece1725a7d
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-11 18:51:40 +0200

    Create doctors_actions.dart

---
+++ src/state/doctors/doctors_actions.dart
@@ -0,0 +1,11 @@
+﻿class LoaddoctorsAction {}
+
+class doctorsLoadedAction {
+  final dynamic data;
+  doctorsLoadedAction(this.data);
+}
+
+class doctorsFailedAction {
+  final String error;
+  doctorsFailedAction(this.error);
+}
