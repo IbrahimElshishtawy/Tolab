b47d28588553556a72d7c82712fd3a792f7006de
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-11 20:33:18 +0200

    Update app_reducer.dart

--- eduhub/lib/apps/tolab_admin_panel/lib/src/state/reducers/app_reducer.dart
+++ eduhub/lib/apps/tolab_admin_panel/lib/src/state/reducers/app_reducer.dart
@@ -1,17 +1,23 @@
+import 'package:eduhub/apps/tolab_admin_panel/lib/src/state/academic_structure/academic_structure_reducer.dart';
 import 'package:eduhub/apps/tolab_admin_panel/lib/src/state/app_state.dart';
 import 'package:eduhub/apps/tolab_admin_panel/lib/src/state/assistants/assistants_reducer.dart';
 import 'package:eduhub/apps/tolab_admin_panel/lib/src/state/auth/auth_reducer.dart';
 import 'package:eduhub/apps/tolab_admin_panel/lib/src/state/dashboard/dashboard_reducer.dart';
 import 'package:eduhub/apps/tolab_admin_panel/lib/src/state/doctors/doctors_reducer.dart';
+import 'package:eduhub/apps/tolab_admin_panel/lib/src/state/permissions/permissions_reducer.dart';
 import 'package:eduhub/apps/tolab_admin_panel/lib/src/state/students/students_reducer.dart';
 
 AppState appReducer(AppState state, dynamic action) {
   return AppState(
     auth: authReducer(state.auth, action),
     dashboard: dashboardReducer(state.dashboard, action),
-    students: studentsReducer(state.students, action), 
-    doctors: doctorsReducer(state.doctors , action) , 
-    assistants: assistantsReducer(state.assistants , action), academicStructure: , permissions:  ,  
-    
+    students: studentsReducer(state.students, action),
+    doctors: doctorsReducer(state.doctors, action),
+    assistants: assistantsReducer(state.assistants, action),
+    academicStructure: academicStructureReducer(
+      state.academicStructure,
+      action,
+    ),
+    permissions: permissionsReducer(state.permissions, action),
   );
 }
