d98ab3135ab29afc03d12d57fec82dcd8c1bfa69
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-11 18:51:55 +0200

    Create doctors_state.dart

---
+++ src/state/doctors/doctors_state.dart
@@ -0,0 +1,22 @@
+﻿class doctorsState {
+  final bool isLoading;
+  final dynamic data;
+  final String? error;
+
+  doctorsState({
+    required this.isLoading,
+    required this.data,
+    this.error,
+  });
+
+  factory doctorsState.initial() =>
+      doctorsState(isLoading: false, data: null);
+
+  doctorsState copyWith({bool? isLoading, dynamic data, String? error}) {
+    return doctorsState(
+      isLoading: isLoading ?? this.isLoading,
+      data: data ?? this.data,
+      error: error,
+    );
+  }
+}
