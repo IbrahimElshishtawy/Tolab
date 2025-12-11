4c47369a727ac5f6da2e8ecb0a8daa7fcda2a342
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-11 18:51:01 +0200

    Create structure_state.dart

---
+++ src/state/academic_structure/structure_state.dart
@@ -0,0 +1,22 @@
+﻿class structureState {
+  final bool isLoading;
+  final dynamic data;
+  final String? error;
+
+  structureState({
+    required this.isLoading,
+    required this.data,
+    this.error,
+  });
+
+  factory structureState.initial() =>
+      structureState(isLoading: false, data: null);
+
+  structureState copyWith({bool? isLoading, dynamic data, String? error}) {
+    return structureState(
+      isLoading: isLoading ?? this.isLoading,
+      data: data ?? this.data,
+      error: error,
+    );
+  }
+}
