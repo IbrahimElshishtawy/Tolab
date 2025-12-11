60f6e0c151c61d28866844382ca4d5036dd03ef1
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-11 18:50:49 +0200

    Create structure_actions.dart

---
+++ src/state/academic_structure/structure_actions.dart
@@ -0,0 +1,11 @@
+﻿class LoadstructureAction {}
+
+class structureLoadedAction {
+  final dynamic data;
+  structureLoadedAction(this.data);
+}
+
+class structureFailedAction {
+  final String error;
+  structureFailedAction(this.error);
+}
