9ff34bc7f62f3ebad7f6d986f30a01ea1103ca9e
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-11 18:51:58 +0200

    Create permissions_actions.dart

---
+++ src/state/permissions/permissions_actions.dart
@@ -0,0 +1,11 @@
+﻿class LoadpermissionsAction {}
+
+class permissionsLoadedAction {
+  final dynamic data;
+  permissionsLoadedAction(this.data);
+}
+
+class permissionsFailedAction {
+  final String error;
+  permissionsFailedAction(this.error);
+}
