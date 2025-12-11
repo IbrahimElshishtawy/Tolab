3d3898e1961177af26b732c8dd5e5e58b762feec
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-11 18:50:06 +0200

    Update students_reducer.dart

--- eduhub/lib/apps/tolab_admin_panel/lib/src/state/students/students_reducer.dart
+++ eduhub/lib/apps/tolab_admin_panel/lib/src/state/students/students_reducer.dart
@@ -1,18 +1,21 @@
-import 'students_state.dart';
+import 'package:redux/redux.dart';
 import 'students_actions.dart';
+import 'students_state.dart';
 
-StudentsState studentsReducer(StudentsState state, dynamic action) {
-  if (action is LoadStudentsAction) {
-    return state.copyWith(isLoading: true);
-  }
+final studentsReducer = combineReducers<StudentsState>([
+  TypedReducer<StudentsState, LoadStudentsAction>(_onLoad).call,
+  TypedReducer<StudentsState, StudentsLoadedAction>(_onLoaded).call,
+  TypedReducer<StudentsState, StudentsFailedAction>(_onFailed).call,
+]);
 
-  if (action is StudentsLoadedAction) {
-    return state.copyWith(isLoading: false, students: action.students);
-  }
+StudentsState _onLoad(StudentsState state, LoadStudentsAction action) {
+  return state.copyWith(isLoading: true, error: null);
+}
 
-  if (action is StudentsFailedAction) {
-    return state.copyWith(isLoading: false, error: action.error);
-  }
+StudentsState _onLoaded(StudentsState state, StudentsLoadedAction action) {
+  return state.copyWith(isLoading: false, students: action.data);
+}
 
-  return state;
+StudentsState _onFailed(StudentsState state, StudentsFailedAction action) {
+  return state.copyWith(isLoading: false, error: action.error);
 }
