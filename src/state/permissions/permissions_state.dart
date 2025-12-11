ae240dd6c7dd4ac5e29af281bd4d7291ea2deb90
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-11 18:52:14 +0200

    Create permissions_state.dart

---
+++ src/state/permissions/permissions_state.dart
@@ -0,0 +1,22 @@
+﻿class permissionsState {
+  final bool isLoading;
+  final dynamic data;
+  final String? error;
+
+  permissionsState({
+    required this.isLoading,
+    required this.data,
+    this.error,
+  });
+
+  factory permissionsState.initial() =>
+      permissionsState(isLoading: false, data: null);
+
+  permissionsState copyWith({bool? isLoading, dynamic data, String? error}) {
+    return permissionsState(
+      isLoading: isLoading ?? this.isLoading,
+      data: data ?? this.data,
+      error: error,
+    );
+  }
+}
