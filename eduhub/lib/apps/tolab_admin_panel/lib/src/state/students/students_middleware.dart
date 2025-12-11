bcb5da3958cbef26876d8073d7eb117c1efe1fd4
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-11 18:50:03 +0200

    Update students_middleware.dart

--- eduhub/lib/apps/tolab_admin_panel/lib/src/state/students/students_middleware.dart
+++ eduhub/lib/apps/tolab_admin_panel/lib/src/state/students/students_middleware.dart
@@ -1,21 +1,19 @@
-import 'package:eduhub/fake_data/data.dart';
 import 'package:redux/redux.dart';
-
+import '../../core/api/api_service_students.dart';
 import '../app_state.dart';
 import 'students_actions.dart';
 
-List<Middleware<AppState>> studentsMiddleware() {
-  return [TypedMiddleware<AppState, LoadStudentsAction>(_loadStudents()).call];
+List<Middleware<AppState>> createStudentsMiddleware(ApiServiceStudents api) {
+  return [TypedMiddleware<AppState, LoadStudentsAction>(_loadStudents(api))];
 }
 
-Middleware<AppState> _loadStudents() {
-  return (store, action, next) async {
+Middleware<AppState> _loadStudents(ApiServiceStudents api) {
+  return (Store<AppState> store, action, NextDispatcher next) async {
     next(action);
 
     try {
-      await Future.delayed(const Duration(milliseconds: 300));
-
-      store.dispatch(StudentsLoadedAction(students));
+      final data = await api.fetchStudents();
+      store.dispatch(StudentsLoadedAction(data));
     } catch (e) {
       store.dispatch(StudentsFailedAction(e.toString()));
     }
