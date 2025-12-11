ce1b97eaa908390c20e42beece724cf20511e153
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-11 21:01:54 +0200

    Update academic_structure_page.dart

--- eduhub/lib/apps/tolab_admin_panel/lib/src/presentation/features/academic_structure/pages/academic_structure_page.dart
+++ eduhub/lib/apps/tolab_admin_panel/lib/src/presentation/features/academic_structure/pages/academic_structure_page.dart
@@ -1,4 +1,6 @@
-﻿import 'package:flutter/material.dart';
+﻿// ignore_for_file: camel_case_types
+
+import 'package:flutter/material.dart';
 
 class academic_structure_pagePage extends StatelessWidget {
   const academic_structure_pagePage({super.key});
@@ -9,10 +11,16 @@ class academic_structure_pagePage extends StatelessWidget {
       backgroundColor: const Color(0xFF0F172A),
       appBar: AppBar(
         backgroundColor: const Color(0xFF1E293B),
-        title: const Text('academic_structure_page', style: TextStyle(color: Colors.white)),
+        title: const Text(
+          'academic_structure_page',
+          style: TextStyle(color: Colors.white),
+        ),
       ),
       body: const Center(
-        child: Text('academic_structure_page Page', style: TextStyle(color: Colors.white)),
+        child: Text(
+          'academic_structure_page Page',
+          style: TextStyle(color: Colors.white),
+        ),
       ),
     );
   }
