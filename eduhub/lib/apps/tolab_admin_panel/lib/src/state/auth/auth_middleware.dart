ab3eb302dd985167509dbe257212552ddb3965df
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-12 22:15:34 +0200

    Update auth_middleware.dart

--- eduhub/lib/apps/tolab_admin_panel/lib/src/state/auth/auth_middleware.dart
+++ eduhub/lib/apps/tolab_admin_panel/lib/src/state/auth/auth_middleware.dart
@@ -1,8 +1,10 @@
-import 'package:eduhub/apps/tolab_admin_panel/lib/src/core/api/Api_Service_auth.dart';
 import 'package:redux/redux.dart';
+import '../app_state.dart';
+import '../../core/api/Api_Service_auth.dart';
 import 'auth_actions.dart';
 
-import '../app_state.dart';
+import '../permissions/permissions_actions.dart';
+import '../dashboard/dashboard_actions.dart';
 
 List<Middleware<AppState>> authMiddleware(ApiServiceAuth api) {
   return [TypedMiddleware<AppState, LoginAction>(_login(api)).call];
@@ -15,7 +17,10 @@ Middleware<AppState> _login(ApiServiceAuth api) {
     try {
       final token = await api.login(action.email, action.password);
 
-      store.dispatch(LoginSuccessAction(token));
+      store.dispatch(LoginSuccessAction(token: token, email: action.email));
+
+      store.dispatch(LoadPermissionsAction());
+      store.dispatch(LoadDashboardDataAction());
     } catch (e) {
       store.dispatch(LoginFailedAction(e.toString()));
     }
