adeaae340dc874d4478a919b200215fcd88a37a3
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-12-13 02:41:37 +0200

    Update students_state.dart

--- eduhub/lib/apps/tolab_admin_panel/lib/src/state/students/students_state.dart
+++ eduhub/lib/apps/tolab_admin_panel/lib/src/state/students/students_state.dart
@@ -5,26 +5,45 @@ class StudentsState {
   final bool isLoading;
   final List<Map<String, dynamic>> students;
   final String? error;
+  final String? department;
+  final int? year;
+  final String? programType;
 
   const StudentsState({
     required this.isLoading,
     required this.students,
     this.error,
+    this.department,
+    this.year,
+    this.programType,
   });
 
   factory StudentsState.initial() {
-    return const StudentsState(isLoading: false, students: [], error: null);
+    return const StudentsState(
+      isLoading: false,
+      students: [],
+      error: null,
+      programType: null,
+      department: null,
+      year: null,
+    );
   }
 
   StudentsState copyWith({
     bool? isLoading,
     List<Map<String, dynamic>>? students,
     String? error,
+    String? programType,
+    String? department,
+    int? year,
   }) {
     return StudentsState(
       isLoading: isLoading ?? this.isLoading,
       students: students ?? this.students,
       error: error,
+      programType: programType ?? this.programType,
+      department: department ?? this.department,
+      year: year ?? this.year,
     );
   }
 }
