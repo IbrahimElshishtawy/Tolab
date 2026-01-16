d0dce338a398b40728c76588b1439b959c2f4e3f
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-01-16 23:26:29 +0200

    Create adaptive_splash_background.dart

---
+++ tolab_fci/lib/features/splash/presentation/widgets/adaptive_splash_background.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+import 'splash_shapes_painter.dart';
+
+class AdaptiveSplashBackground extends StatelessWidget {
+  final Widget child;
+
+  const AdaptiveSplashBackground({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+
+    final backgroundColor = isDark ? const Color(0xFF023EC5) : Colors.white;
+
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: Stack(
+        children: [
+          CustomPaint(size: Size.infinite, painter: SplashShapesPainter()),
+          child,
+        ],
+      ),
+    );
+  }
+}
