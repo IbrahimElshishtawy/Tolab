e2facf87975595135ca6bc47a614e2804fb05368
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-01-17 00:03:27 +0200

    Update splash_screen.dart

--- tolab_fci/lib/features/splash/presentation/screens/splash_screen.dart
+++ tolab_fci/lib/features/splash/presentation/screens/splash_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unnecessary_underscores
+
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:tolab_fci/features/splash/presentation/widgets/Tolab_Logo_Icon_write(T).dart';
