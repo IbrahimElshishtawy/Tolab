5d3377eb83e2577bdc25e5d22d0b83747e7d441c
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-01-16 23:52:31 +0200

    Update splash_screen.dart

--- tolab_fci/lib/features/splash/presentation/screens/splash_screen.dart
+++ tolab_fci/lib/features/splash/presentation/screens/splash_screen.dart
@@ -1,31 +1,55 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:tolab_fci/features/splash/presentation/widgets/Tolab_Logo_Icon_write(T).dart';
 import 'package:tolab_fci/features/splash/presentation/widgets/splash_background.dart';
+import 'package:tolab_fci/features/splash/presentation/screens/intro_screen.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 2), _goToIntro);
+  }
+
+  void _goToIntro() {
+    Navigator.of(context).pushReplacement(
+      PageRouteBuilder(
+        transitionDuration: const Duration(milliseconds: 800),
+        pageBuilder: (_, __, ___) => const IntroScreen(),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return SplashBackground(
       child: Center(
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            // 🔲 White square with rotated T
-            TolabLogoBox(),
-            const SizedBox(width: 7),
-
-            const Text(
-              'OLAB',
-              style: TextStyle(
-                fontSize: 28,
-                letterSpacing: 2,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+        child: Hero(
+          tag: 'tolab_logo',
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TolabLogoBox(),
+              const SizedBox(width: 7),
+              const Text(
+                'OLAB',
+                style: TextStyle(
+                  fontSize: 28,
+                  letterSpacing: 2,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
