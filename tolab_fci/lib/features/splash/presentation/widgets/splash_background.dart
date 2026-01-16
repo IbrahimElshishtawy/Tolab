e2a8300e3794c92f82fdeefcfb00f1f33b8141c9
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-01-16 19:04:21 +0200

    Update splash_background.dart

---
+++ tolab_fci/lib/features/splash/presentation/widgets/splash_background.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:tolab_fci/features/splash/presentation/widgets/Splash_Shapes_Painter.dart';
+
+class SplashBackground extends StatelessWidget {
+  final Widget child;
+
+  const SplashBackground({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF0B4DFF),
+      body: Stack(
+        children: [
+          CustomPaint(size: Size.infinite, painter: SplashShapesPainter()),
+          child,
+        ],
+      ),
+    );
+  }
+}
