b6e97759af0c26d1656392f4022223891fb42d96
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-01-16 19:04:14 +0200

    Update main.dart

--- tolab_fci/lib/main.dart
+++ tolab_fci/lib/main.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'features/splash/presentation/screens/splash_screen.dart';
 
-void main() => runApp(app());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
 
-class app extends StatelessWidget {
-  const app({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'OLAB',
+      theme: ThemeData(
+        fontFamily: 'Cairo', // لو بتستخدم خط عربي
+        useMaterial3: true,
+      ),
+      home: const SplashScreen(),
+    );
   }
 }
