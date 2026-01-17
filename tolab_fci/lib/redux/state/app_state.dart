3c25184371c48d83ccd7c7743012fa0317952a28
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-01-17 13:41:10 +0200

    Update app_state.dart

--- tolab_fci/lib/redux/state/app_state.dart
+++ tolab_fci/lib/redux/state/app_state.dart
@@ -1,14 +1,23 @@
 import 'auth_state.dart';
+import 'ui_state.dart';
 
 class AppState {
   final AuthState authState;
+  final UIState uiState;
 
-  AppState({required this.authState});
+  AppState({required this.authState, required this.uiState});
+
   factory AppState.initial() {
-    return AppState(authState: AuthState.initial());
+    return AppState(
+      authState: AuthState(isAuthenticated: false, isLoading: false),
+      uiState: UIState.initial(),
+    );
   }
 
-  AppState copyWith({AuthState? authState}) {
-    return AppState(authState: authState ?? this.authState);
+  AppState copyWith({AuthState? authState, UIState? uiState}) {
+    return AppState(
+      authState: authState ?? this.authState,
+      uiState: uiState ?? this.uiState,
+    );
   }
 }
