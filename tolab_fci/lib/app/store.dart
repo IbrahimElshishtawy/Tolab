ce5eacb8bbb68060c2f810a47296f7429c762347
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-01-17 20:41:27 +0200

    Update store.dart

--- tolab_fci/lib/app/store.dart
+++ tolab_fci/lib/app/store.dart
@@ -1,12 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:redux/redux.dart';
+import 'package:tolab_fci/features/auth/data/datasources/auth_remote_ds.dart';
+import 'package:tolab_fci/features/auth/data/datasources/auth_role_ds.dart';
+import 'package:tolab_fci/features/auth/data/repositories/auth_repository_impl.dart';
+import 'package:tolab_fci/redux/middlewares/auth_middleware.dart';
 import 'package:tolab_fci/redux/reducers/root_reducer.dart';
-import 'middlewares.dart';
-import '../redux/state/app_state.dart';
+import 'package:tolab_fci/redux/state/app_state.dart';
 
 Store<AppState> createStore() {
+  final firebaseAuth = FirebaseAuth.instance;
+
+  final AuthRemoteDataSource remoteDS = AuthRemoteDataSourceImpl(firebaseAuth);
+
+  final AuthRoleDataSource roleDS = AuthRoleDataSourceImpl(
+    firebaseAuth as FirebaseFirestore,
+  );
+
+  final authRepository = AuthRepositoryImpl(
+    remoteDataSource: remoteDS,
+    roleDataSource: roleDS,
+  );
+
   return Store<AppState>(
     appReducer,
     initialState: AppState.initial(),
-    middleware: createAppMiddlewares(),
+    middleware: [...createAuthMiddleware(authRepository)],
   );
 }
