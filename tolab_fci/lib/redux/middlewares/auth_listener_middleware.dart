8250afc58bed4863e742f5092924d020c39c0516
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-01-18 20:34:28 +0200

    Update auth_listener_middleware.dart

--- tolab_fci/lib/redux/middlewares/auth_listener_middleware.dart
+++ tolab_fci/lib/redux/middlewares/auth_listener_middleware.dart
@@ -4,16 +4,18 @@ import '../actions/auth_actions.dart';
 import '../state/app_state.dart';
 
 Middleware<AppState> createAuthListenerMiddleware(FirebaseAuth firebaseAuth) {
-  return (store, action, next) {
+  return (Store<AppState> store, action, NextDispatcher next) {
     next(action);
 
     firebaseAuth.authStateChanges().listen((user) {
-      if (user != null && user.email != null) {
+      if (user == null) {
+        store.dispatch(const LogoutAction());
+      } else {
         store.dispatch(
           LoginSuccessAction(
             uid: user.uid,
-            email: user.email!,
-            role: store.state.authState.role ?? '',
+            email: user.email ?? '',
+            role: store.state.authState.role ?? 'student',
           ),
         );
       }
