0f5e097f1c9f13aa2d89062ce9c6cd01b9ed339e
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-01-18 19:24:22 +0200

    Create it_home_screen.dart

---
+++ tolab_fci/lib/features/home/presentation/screens/it_home_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ItHomeScreen extends StatelessWidget {
+  const ItHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Center(child: Text('IT Home')));
+  }
+}
