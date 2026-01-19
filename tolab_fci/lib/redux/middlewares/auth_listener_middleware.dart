6259935c409ac9188017063dcdff066c4839abb7
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-01-19 20:11:46 +0200

    Update auth_listener_middleware.dart

--- tolab_fci/lib/redux/middlewares/auth_listener_middleware.dart
+++ tolab_fci/lib/redux/middlewares/auth_listener_middleware.dart
@@ -1,26 +1,27 @@
-// auth_listener_middleware.dart
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/foundation.dart';
 import 'package:redux/redux.dart';
 import '../actions/auth_actions.dart';
 import '../state/app_state.dart';
 
 Middleware<AppState> createAuthListenerMiddleware(FirebaseAuth firebaseAuth) {
-  return (store, action, next) {
+  return (Store<AppState> store, action, NextDispatcher next) {
     next(action);
 
     firebaseAuth.authStateChanges().listen((user) {
-      debugPrint('🔥 AUTH LISTENER USER: $user');
-
-      if (user != null && user.email != null) {
-        store.dispatch(
-          LoginSuccessAction(
-            uid: user.uid,
-            email: user.email!,
-            role: store.state.authState.role ?? 'student',
-          ),
-        );
+      if (user == null) {
+        if (store.state.authState.isAuthenticated) {
+          store.dispatch(const LogoutAction());
+        }
+        return;
       }
+      if (store.state.authState.isAuthenticated) return;
+      store.dispatch(
+        LoginSuccessAction(
+          uid: user.uid,
+          email: user.email ?? '',
+          role: store.state.authState.role ?? 'student',
+        ),
+      );
     });
   };
 }
