9a889ed80bc783899578834575fbc2b5606bf421
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-01-19 23:18:30 +0200

    Update main.dart

--- tolab_fci/lib/main.dart
+++ tolab_fci/lib/main.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_redux/flutter_redux.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-import 'firebase_options.dart';
 import 'app/app.dart';
 import 'app/store.dart';
-
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  final store = createStore();
 
-  runApp(StoreProvider(store: createStore(), child: const App()));
+  runApp(App(store: store));
 }
