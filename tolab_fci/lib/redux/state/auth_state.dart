a02861f9c9ee5de6a5fa734c9c041a3f66102089
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-01-21 00:51:28 +0200

    Update auth_state.dart

--- tolab_fci/lib/redux/state/auth_state.dart
+++ tolab_fci/lib/redux/state/auth_state.dart
@@ -1,46 +1,18 @@
 class AuthState {
-  final bool isAuthenticated;
   final bool isLoading;
   final String? uid;
   final String? email;
   final String? role;
-  final String? error;
 
   AuthState({
-    required this.isAuthenticated,
     required this.isLoading,
     this.uid,
     this.email,
     this.role,
-    this.error,
+    required bool isAuthenticated,
+    required error,
   });
 
-  factory AuthState.initial() {
-    return AuthState(
-      isAuthenticated: false,
-      isLoading: false,
-      uid: null,
-      email: null,
-      role: null,
-      error: null,
-    );
-  }
-
-  AuthState copyWith({
-    bool? isAuthenticated,
-    bool? isLoading,
-    String? uid,
-    String? email,
-    String? role,
-    String? error,
-  }) {
-    return AuthState(
-      isAuthenticated: isAuthenticated ?? this.isAuthenticated,
-      isLoading: isLoading ?? this.isLoading,
-      uid: uid ?? this.uid,
-      email: email ?? this.email,
-      role: role ?? this.role,
-      error: error,
-    );
-  }
+  factory AuthState.initial() =>
+      AuthState(isLoading: false, isAuthenticated: false, error: null);
 }
