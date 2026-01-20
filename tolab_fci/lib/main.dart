0ca0a941f20c89bab11c581dd86ef16e5d2f5308
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-01-21 00:51:12 +0200

    Update main.dart

--- tolab_fci/lib/main.dart
+++ tolab_fci/lib/main.dart
@@ -3,11 +3,10 @@ import 'package:firebase_core/firebase_core.dart';
 
 import 'app/app.dart';
 import 'app/store.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-
   final store = createStore();
-
   runApp(App(store: store));
 }
