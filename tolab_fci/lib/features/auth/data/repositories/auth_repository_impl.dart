bece4bb03153bed9157538d9a0800c1b052a30a4
Eng Ibrahim Elshishtawy <190879691+IbrahimElshishtawy@users.noreply.github.com>
2026-01-21 00:51:03 +0200

    Update auth_repository_impl.dart

--- tolab_fci/lib/features/auth/data/repositories/auth_repository_impl.dart
+++ tolab_fci/lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -1,60 +1,23 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:tolab_fci/features/auth/data/repositories/auth_repository.dart';
 import '../datasources/auth_remote_ds.dart';
-import '../datasources/auth_role_ds.dart';
 
 class AuthRepositoryImpl implements AuthRepository {
   final AuthRemoteDataSource remoteDataSource;
-  final AuthRoleDataSource roleDataSource;
-  final FirebaseFirestore firestore;
 
-  AuthRepositoryImpl({
-    required this.remoteDataSource,
-    required this.roleDataSource,
-    required this.firestore,
-  });
+  AuthRepositoryImpl({required this.remoteDataSource});
 
-  /// ===============================
-  /// 🔍 Check if email is registered
-  /// ===============================
-  @override
-  Future<bool> isEmailRegistered(String email) async {
-    final normalizedEmail = email.toLowerCase().trim();
-
-    final query = await firestore
-        .collection('users')
-        .where('email', isEqualTo: normalizedEmail)
-        .limit(1)
-        .get();
-
-    return query.docs.isNotEmpty;
-  }
-
-  /// ===============================
-  /// 🔐 Microsoft Sign In
-  /// ===============================
   @override
   Future<void> signInWithMicrosoft(String selectedRole) async {
-    // ❗ لا نحدد Role هنا
-    // ❗ لا نعمل LoginSuccess هنا
-    // Firebase Auth Listener هو المسؤول
-
     await remoteDataSource.signInWithMicrosoft();
   }
 
-  /// ===============================
-  /// 👤 Current Firebase User
-  /// ===============================
   @override
   User? getCurrentUser() {
     return remoteDataSource.getCurrentUser();
   }
 
-  /// ===============================
-  /// 🚪 Logout
-  /// ===============================
   @override
   Future<void> signOut() async {
     await remoteDataSource.signOut();
